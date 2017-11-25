218d2fce42bcc03a3bd089b73214016c54e4d6e6
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2017-11-25 16:26:04 +0900

    プログラミング２　report4

--- src/main.java
+++ src/main.java
@@ -1,8 +1,11 @@
 public class main {
     public static void main(String[] argc) {
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            Double value =0.00;
+            value.parseDouble(str);
+            System.out.println(str);
+
         } catch (NullPointerException e) {
             //NullPointerExceptionが起きたときの処理
             System.out.println(e.getMessage());
